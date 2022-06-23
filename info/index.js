3a730c3d9f470d254b260805341bb56c3f4fc2aa
conradgabe <gisuekebho5880@gmail.com>
2022-06-23 08:52:10 -1100

    js info

---
+++ info/index.js
@@ -0,0 +1,9 @@
+alert('Gabriel Isuekebho,', '5ft 9inches,', 'Nigeria');
+
+let firstName = prompt('What is your name? ');
+let height = prompt('What is your height? ');
+let country = prompt('Where are you from? ');
+
+alert(firstName + ' ' +  height + ' ' + country);
+
+console.log(firstName, height, country);
